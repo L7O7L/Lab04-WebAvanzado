1ba8579f6c96784d56633438b56bf828925f0e42
Luis <luisbaltazarsegura@gmail.com>
2023-04-24 12:02:44 -0500

    tercer cambio

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+var path = require('path')
 
 const http = require('http')
 const server = http.createServer(app)
 
 const {Server} = require('socket.io')
 const io = new Server(server)
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
 
